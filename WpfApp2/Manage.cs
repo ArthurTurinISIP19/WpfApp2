d39d769e3f5cca1759d62bf39f474d88a389367a
Turin Arthur <ezeka1n@mail.ru>
2021-09-27 11:08:14 +0900

    Добавьте файлы проекта.

---
+++ WpfApp2/Manage.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Controls;
+
+namespace WpfApp2
+{
+    class Manage
+    {
+        public static Frame MainFrame { get; set; }
+    }
+}
